49e55ff091086e846390e590c03f322c7e087469
UrieliasCB <uri@corebooks.com>
2022-03-22 13:24:30 -0600

    fixes

--- components/AreaGraph/AreaGraph.tsx
+++ components/AreaGraph/AreaGraph.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import PropTypes from "prop-types";
 import "./AreaGraph.css";
 import { Area, AreaChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
@@ -33,7 +33,7 @@ const CustomTooltip = ({
 					<path
 						d="M71.4958 67.2928L53.3304 49.3367L53.1843 49.1923H52.9789H10C4.7533 49.1923 0.5 44.939 0.5 39.6923V10C0.5 4.7533 4.7533 0.5 10 0.5H133C138.247 0.5 142.5 4.7533 142.5 10V39.6923C142.5 44.939 138.247 49.1923 133 49.1923H89.5904H89.3815L89.2347 49.3409L71.4958 67.2928Z"
 						fill="url(#paint0_linear_4229_20606)"
-						fill-opacity="0.4"
+						fillOpacity="0.4"
 						stroke="url(#paint1_linear_4229_20606)"
 					/>
 					<defs>
@@ -45,8 +45,8 @@ const CustomTooltip = ({
 							y2="68"
 							gradientUnits="userSpaceOnUse"
 						>
-							<stop stop-color="#514379" />
-							<stop offset="1" stop-color="#13111A" />
+							<stop stopColor="#514379" />
+							<stop offset="1" stopColor="#13111A" />
 						</linearGradient>
 						<linearGradient
 							id="paint1_linear_4229_20606"
@@ -56,8 +56,8 @@ const CustomTooltip = ({
 							y2="68"
 							gradientUnits="userSpaceOnUse"
 						>
-							<stop stop-color="white" />
-							<stop offset="1" stop-color="white" stop-opacity="0" />
+							<stop stopColor="white" />
+							<stop offset="1" stopColor="white" stopOpacity="0" />
 						</linearGradient>
 					</defs>
 				</svg>
@@ -76,17 +76,7 @@ const AreaGraph = ({ data }: IProps) => {
 	const [smallest, setSmallest] = useState<number>(0);
 	const [biggest, setBiggest] = useState<number>(0);
 
-	useEffect(() => {
-		setPoints(undefined);
-		setCoords(undefined);
-		const sorted = data
-			.map((entry) => {
-				return entry.value;
-			})
-			.sort((a, b) => a - b);
-		setSmallest(sorted[0]);
-		setBiggest(sorted[sorted.length - 1]);
-	}, [data]);
+	const ref = useRef<HTMLDivElement>(null);
 
 	const updateIndex = (chart: CategoricalChartState) => {
 		let i = chart.activeTooltipIndex;
@@ -98,49 +88,93 @@ const AreaGraph = ({ data }: IProps) => {
 		setCoords({ x: points[i].x, y: points[i].y });
 	};
 
+	const mapRange = (
+		val: number,
+		inStart: number,
+		inEnd: number,
+		outStart: number,
+		outEnd: number
+	) => outStart + ((outEnd - outStart) / (inEnd - inStart)) * (val - inStart);
+
+	const mapPoints = () => {
+		let sorted = data
+			.map((entry) => {
+				return entry.value;
+			})
+			.sort((a, b) => a - b);
+		let smallest = sorted[0];
+		let biggest = sorted[sorted.length - 1];
+		let pointsData = data.map((entry, index) => {
+			if (!ref.current || !ref.current.offsetWidth) return { x: 0, y: 0 };
+			return {
+				x: ((index + 1) * ref.current.offsetWidth) / (data.length + 1),
+				y: mapRange(entry.value, smallest, biggest, ref.current.offsetHeight, 0),
+			};
+		});
+		return [
+			{ x: 0, y: pointsData[0].y },
+			...pointsData,
+			ref.current
+				? { x: ref.current.offsetWidth, y: pointsData[pointsData.length - 1].y }
+				: { x: 0, y: 0 },
+		];
+	};
+
+	useEffect(() => {
+		setCoords(undefined);
+		const sorted = data
+			.map((entry) => {
+				return entry.value;
+			})
+			.sort((a, b) => a - b);
+		setSmallest(sorted[0]);
+		setBiggest(sorted[sorted.length - 1]);
+		setPoints(mapPoints());
+	}, [data, ref.current]);
+
+	console.log(coords);
 	return (
-		<ResponsiveContainer width="100%" height="100%">
-			<AreaChart
-				data={[
-					{ name: "extra", value: [smallest, data[0].value] },
-					...data.map((entry) => {
-						return { ...entry, value: [smallest, entry.value] };
-					}),
-					{ name: "extra", value: [smallest, data[data.length - 1].value] },
-				]}
-				margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
-				onMouseMove={(chart) => updateIndex(chart)}
-				onMouseLeave={() => setCoords(undefined)}
-			>
-				<defs>
-					<linearGradient id="inner-gradient" gradientTransform="rotate(90)">
-						<stop offset="0" stopColor="#3370f5" />
-						<stop offset="1" stopColor="#ee5aa2" />
-					</linearGradient>
-				</defs>
-				<XAxis hide dataKey="name" />
-				<YAxis hide type="number" domain={[smallest, biggest]} />
-				<Tooltip
-					content={<CustomTooltip />}
-					cursor={false}
-					isAnimationActive={false}
-					wrapperStyle={{ display: coords ? "inline" : "none" }}
-					position={coords ? coords : { x: 0, y: 0 }}
-				/>
-				<Area
-					activeDot={false}
-					type="linear"
-					dataKey="value"
-					stroke="#fff"
-					strokeWidth={2}
-					fillOpacity={1}
-					fill="url(#inner-gradient)"
-					onMouseOver={(data: any) => {
-						if (!points) setPoints(data.points);
-					}}
-				/>
-			</AreaChart>
-		</ResponsiveContainer>
+		<div style={{ width: "100%", height: "100%" }} ref={ref}>
+			<ResponsiveContainer width="100%" height="100%">
+				<AreaChart
+					data={[
+						{ name: "extra", value: [smallest, data[0].value] },
+						...data.map((entry) => {
+							return { ...entry, value: [smallest, entry.value] };
+						}),
+						{ name: "extra", value: [smallest, data[data.length - 1].value] },
+					]}
+					margin={{ top: 0, left: 0, right: 0, bottom: 0 }}
+					onMouseMove={(chart) => updateIndex(chart)}
+					onMouseLeave={() => setCoords(undefined)}
+				>
+					<defs>
+						<linearGradient id="inner-gradient" gradientTransform="rotate(90)">
+							<stop offset="0" stopColor="#3370f5" />
+							<stop offset="1" stopColor="#ee5aa2" />
+						</linearGradient>
+					</defs>
+					<XAxis hide dataKey="name" />
+					<YAxis hide type="number" domain={[smallest, biggest]} />
+					<Tooltip
+						content={<CustomTooltip />}
+						cursor={false}
+						isAnimationActive={false}
+						wrapperStyle={{ display: coords ? "inline" : "none" }}
+						position={coords ? coords : { x: 0, y: 0 }}
+					/>
+					<Area
+						activeDot={false}
+						type="linear"
+						dataKey="value"
+						stroke="#fff"
+						strokeWidth={2}
+						fillOpacity={1}
+						fill="url(#inner-gradient)"
+					/>
+				</AreaChart>
+			</ResponsiveContainer>
+		</div>
 	);
 };
 
