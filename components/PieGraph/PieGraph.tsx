7e5145df5c8c3723a7b8a4a401b81ab558eaac6c
Iván Peña <ivan@corebooks.com>
2022-05-19 11:54:22 -0500

    Cambios visibilidad

--- components/PieGraph/PieGraph.tsx
+++ components/PieGraph/PieGraph.tsx
@@ -11,29 +11,37 @@ interface IProps {
 	}>;
 }
 
-const COLORS = ["#FF4895", "#6E4FE9", "#1766FF", "#2D42FF"];
-
-const RADIAN = Math.PI / 180;
 const renderCustomizedLabel = (props: any) => {
-	const { cx, cy, midAngle, outerRadius, percentage } = props;
-	const radius = outerRadius * 1.5;
-	const x = cx + radius * Math.cos(-midAngle * RADIAN);
-	const y = cy + radius * Math.sin(-midAngle * RADIAN);
-
-	return (
-		<text
-			x={x}
-			y={y}
-			fill="white"
-			textAnchor="middle"
-			dominantBaseline="central"
-			className="custom-label"
-		>
-			<tspan x={x} y={y - 10} style={{ fontWeight: 400 }}>{`${percentage[0]}`}</tspan>
-			<tspan x={x} y={y + 10} style={{ fontWeight: 800 }}>{`${percentage[1]}`}</tspan>
-		</text>
-	);
+	let { cx, cy, midAngle, innerRadius, outerRadius, value, color, startAngle, endAngle, percentage } = props;
+    const RADIAN = Math.PI / 180;
+    const diffAngle = endAngle - startAngle;
+    const delta = ((360-diffAngle)/15)-1;
+    const radius = innerRadius + (outerRadius - innerRadius);
+    const x = cx + (radius+delta) * Math.cos(-midAngle * RADIAN);
+    const y = cy + (radius+(delta*delta)) * Math.sin(-midAngle * RADIAN);
+    return (
+      <text x={x} y={y} fill={color} textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central" fontWeight="normal">
+		<tspan x={x} y={y - 10} style={{ fontWeight: 400 }}>{`${percentage[0]}`}</tspan>
+		<tspan x={x} y={y + 10} style={{ fontWeight: 800 }}>{`${percentage[1]}`}</tspan>
+      </text>
+    );
 };
+
+const renderCustomizedLabelLine = (props: any) => {
+    let { cx, cy, midAngle, innerRadius, outerRadius, color, startAngle, endAngle } = props;
+    const RADIAN = Math.PI / 180;
+    const diffAngle = endAngle - startAngle;
+    const radius = innerRadius + (outerRadius - innerRadius);
+    let path='';
+    for(let i=0;i<((360-diffAngle)/15);i++){
+      path += `${(cx + (radius+i) * Math.cos(-midAngle * RADIAN))},${(cy + (radius+i*i) * Math.sin(-midAngle * RADIAN))} `
+    }
+    return (
+      <polyline points={path} stroke={color} fill="none" />
+    );
+};
+
+const COLORS = ["#FF4895", "#6E4FE9", "#1766FF", "#2D42FF"];
 
 const PieGraph = ({ data }: IProps) => {
 	const [data2, setData2] = useState<IProps["data"]>();
